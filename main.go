f8d839f958cd897b763e97966bf91e97f9133ee5
SzMark092 <szmgepesz@gmail.com>
2021-02-19 23:34:41 +0100

    Candy creation added

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	max := 100
+	stepper := 0
+	var Root Candy
+	cn := 0
+	Root.candyNumber = &cn
+	Root.CreateColumn(max, &stepper)
+	fmt.Println("Hello!")
 }
 
 type Candy struct {
 	color          int
 	next           *Candy
 	deleteNextTime bool
+	candyNumber    *int
+}
+
+func (c *Candy) CreateColumn(max int, stepper *int) {
+	c.color = rand.Intn(10)
+	*stepper += 1
+	*c.candyNumber++
+	if *stepper < max {
+		var actCandy Candy
+		c.next = &actCandy
+		c.next.candyNumber = c.candyNumber
+		c.next.CreateColumn(max, stepper)
+	}
 }
 
 func (c *Candy) DeleteDuplicants(startAgain *bool, firstElement Candy) {
@@ -27,5 +51,6 @@ func (c *Candy) DeleteDuplicants(startAgain *bool, firstElement Candy) {
 }
 
 func (c *Candy) DeleteNext() {
+	*c.candyNumber--
 	c.next = c.next.next
 }
